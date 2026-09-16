23c7c6b8be19d37f69bd4bd5424ce51c3d06a24f
sagnik1552003 <sagnikghosh0012@gmail.com>
2026-09-16 20:38:00 +0530

    fix: updated gitignore

--- dist/modules/boards/board.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const board_controller_js_1 = require("./board.controller.js");
-const router = (0, express_1.Router)();
-router.post("/", board_controller_js_1.createBoardController);
-router.get("/:id", board_controller_js_1.getBoardController);
-exports.default = router;
--- dist/modules/tasks/task.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const task_controller_js_1 = require("./task.controller.js");
-const router = (0, express_1.Router)();
-router.post("/", task_controller_js_1.createTaskController);
-router.patch("/:id", task_controller_js_1.updateTaskController);
-router.delete("/:id", task_controller_js_1.deleteTaskController);
-exports.default = router;
